5f81375ed4ce15f41cfd6bd5eb5800154cf68e8a
frievoe97 <friedrich.voelkers@me.com>
2025-11-02 15:34:55 +0100

    add gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+// App.tsx
 import './App.css'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import FullscreenLayout from '@/app/components/FullscreenLayout'
 import MapPage from '@/app/routes/map/MapPage'
 import StatsPage from '@/app/routes/stats/StatsPage'
 
+const basename = import.meta.env.PROD ? '/flight-viz' : '/'
+
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <FullscreenLayout>
         <Routes>
           <Route path="/" element={<Navigate to="/map" replace />} />
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from "vite";
-import { fileURLToPath, URL } from "node:url";
-import react from "@vitejs/plugin-react";
-import tailwind from "@tailwindcss/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+// vite.config.ts
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+import react from '@vitejs/plugin-react'
+import tailwind from '@tailwindcss/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwind(), tsconfigPaths()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-});
+  base: '/flight-viz/', // 👈 wichtig für GitHub Pages
+})
